91bc7de580c20b4db4b14d2ab6a29f2c8ff1c793
Alexander Jahn <alexander.jahn@netlogix.de>
2022-05-24 11:23:44 +0200

    FEATURE: use TYPO3 request to handle bans
    This replaces plain CURL with native TYPO3 request handling. This adds
    better error logging and the possibility for future test cases using
    mocks.

---
+++ Classes/Service/VarnishService.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Netlogix\Nxvarnish\Service;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Http\RequestFactory;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class VarnishService implements LoggerAwareInterface, SingletonInterface
+{
+    use LoggerAwareTrait;
+
+    protected $varnishHost = '';
+
+    public function __construct()
+    {
+        $this->varnishHost = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(
+            'nxvarnish',
+            'varnishHost'
+        );
+    }
+
+    /**
+     * Ban all documents with the given tag from varnish
+     */
+    public function banTag(string $tag): void
+    {
+        try {
+            $response = GeneralUtility::makeInstance(RequestFactory::class)
+                ->request($this->varnishHost, 'BAN', ['headers' => ['X-Cache-Tags' => $tag]]);
+
+            if ($response->getStatusCode() !== 200) {
+                $this->logger->error('unexpected status after purging Varnish cache', ['tag' => $tag, 'status' => $response->getStatusCode()]);
+            }
+
+        } catch (\Exception $e) {
+            $this->logger->error('failed purging Varnish cache', ['exception' => $e, 'tag' => $tag]);
+        }
+    }
+
+}
