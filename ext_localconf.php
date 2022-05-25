f74b41e324eb6d246fb9e6251ecc4afb307aa489
Alexander Jahn <alexander.jahn@netlogix.de>
2022-05-25 09:49:06 +0200

    CLEANUP: run code reformat

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,12 @@
 <?php
 
+use Netlogix\Nxvarnish\Cache\Backend\VarnishBackend;
+use Netlogix\Nxvarnish\Hooks\ContentPostProcHook;
 use Netlogix\Nxvarnish\Xclass\Controller\TypoScriptFrontendController;
 use TYPO3\CMS\Core\Cache\Frontend\VariableFrontend;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController as T3TypoScriptFrontendController;
 
 defined('TYPO3_MODE') or die();
 
@@ -12,15 +15,15 @@ call_user_func(function () {
     // this is used instead of a clearCachePostProc hook to get notified of Install Tool `Flush Cache` action as well
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_nxvarnish_dummy'] = [
         'frontend' => VariableFrontend::class,
-        'backend' => \Netlogix\Nxvarnish\Cache\Backend\VarnishBackend::class,
+        'backend' => VarnishBackend::class,
         'groups' => ['pages', 'all']
     ];
 
     $allowCacheLogin = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('nxvarnish', 'allowCacheLogin');
     if ($allowCacheLogin) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController::class]['className'] = TypoScriptFrontendController::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][T3TypoScriptFrontendController::class]['className'] = TypoScriptFrontendController::class;
     }
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached'][] =
-        \Netlogix\Nxvarnish\Hooks\ContentPostProcHook::class . '->cached';
+        ContentPostProcHook::class . '->cached';
 });
